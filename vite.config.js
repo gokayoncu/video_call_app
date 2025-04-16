f0590a2aade2868617ffe7c6aabdd7d9105e9441
Gökay Öncü <gokayoncu35@gmail.com>
2025-04-16 21:52:48 +0300

    vite and build config edit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copyFileSync } from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
+  closeBundle() {
+    try {
+      copyFileSync(path.resolve(__dirname, 'public/_redirects'), path.resolve(__dirname, 'dist/_redirects'))
+      console.log('✅ _redirects dosyası dist/ içine kopyalandı.')
+    } catch (e) {
+      console.warn('⚠️ _redirects dosyası kopyalanamadı:', e)
+    }
+  }
 })
